d9789d7577b6f8c71cb6d3e102241844cbbcdf23
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-09-01 17:58:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-window.promises = [];
+let promises = [];
+const container = document.getElementById("output");
+
+for(let i=1; i<=5; i++){
+	let randomDuration = Math.floor(Math.random()*5)+1;
+	let prom = new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(`fastest promise ${randomDuration} sec`);
+		},randomDuration*1000);
+	});
+	promises.push(prom);
+}
+
+let result = Promise.any(promises);
+  result.then((data)=>{
+	  container.innerText = data;
+  })
 
 // Do not change the code above this
 // add your promises to the array `promises`
